e47e6b42d1a7e203ab1cf0c3ca1e767c17aa0e7e
chrissetiana <chrissetiana@gmail.com>
2018-05-21 21:07:38 +0100

    Added fragments

---
+++ app/src/main/java/com/example/android/omniana/CategoryAdapter.java
@@ -0,0 +1,22 @@
+package com.example.android.omniana;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
